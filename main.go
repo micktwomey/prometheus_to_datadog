1f07f4773eec632c1f08ad12bc852a494a231009
Jessica Lucci <luccacabra@github.com>
2018-02-13 12:34:24 -0600

    move to dep; switch to go syntax formatting; use config driven settings instead of cli

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"github.com/DataDog/datadog-go/statsd"
-	"github.com/prometheus/client_golang/api/prometheus"
-	prometheus_metrics "github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/common/model"
-	"golang.org/x/net/context"
+	"io/ioutil"
 	"net/http"
 	"strings"
 	"time"
+
+	"golang.org/x/net/context"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/DataDog/datadog-go/statsd"
+	"github.com/prometheus/client_golang/api/prometheus"
+	prometheusMetrics "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/model"
+	"github.com/spf13/viper"
 )
 
-type QueryType int
+type QueryType string
 
 const (
-	Gauge QueryType = iota
-	Counter
-	Histogram
-	Set
-	Milliseconds
+	Gauge        QueryType = "gauge"
+	Counter                = "counter"
+	Histogram              = "histogram"
+	Set                    = "set"
+	Milliseconds           = "milliseconds"
 )
 
 type Query struct {
@@ -35,59 +41,25 @@ func (flags *Queries) String() string {
 	return "Queries"
 }
 
-func (flags *Queries) Set(value string) error {
-	parts := strings.SplitN(value, ":", 3)
-	metric_type := parts[0]
-	metric_name := parts[1]
-	prometheus_query := parts[2]
-
-	var query_type QueryType
-	switch metric_type {
-	case "gauge":
-		query_type = Gauge
-	case "counter":
-		query_type = Counter
-	case "histogram":
-		query_type = Histogram
-	case "set":
-		return fmt.Errorf("Don't know how to handle sets (yet)")
-	case "milliseconds":
-		query_type = Milliseconds
-	default:
-		return fmt.Errorf("Can't handle query type %v (%v)", metric_type, value)
-	}
-
-	*flags = append(*flags, Query{Type: query_type, Name: metric_name, Query: prometheus_query})
-	return nil
-}
-
 var (
-	dogstatsd_addr  = flag.String("dogstatsd-address", "127.0.0.1:8125", "The address to send dogstatsd metrics to.")
-	prometheus_addr = flag.String("prometheus-address", "127.0.0.1:9090", "The prometheus address")
-	listen_addr     = flag.String("listen-address", ":9132", "HTTP address to listen on to publish internal metrics.")
-	interval        = flag.Int("interval", 10, "Frequency to query Prometheus (in seconds)")
-	queries         Queries
-)
-
-var (
-	pushedMetrics = prometheus_metrics.NewCounterVec(
-		prometheus_metrics.CounterOpts{
+	pushedMetrics = prometheusMetrics.NewCounterVec(
+		prometheusMetrics.CounterOpts{
 			Namespace: "prometheus_to_datadog",
 			Name:      "pushed_metrics_total",
 			Help:      "Number of metrics pushed",
 		},
 		[]string{"prometheus_name", "dogstatsd_name", "metric_type"},
 	)
-	failedQueries = prometheus_metrics.NewCounterVec(
-		prometheus_metrics.CounterOpts{
+	failedQueries = prometheusMetrics.NewCounterVec(
+		prometheusMetrics.CounterOpts{
 			Namespace: "prometheus_to_datadog",
 			Name:      "failed_queries_total",
 			Help:      "Number of failed queries to Prometheus",
 		},
 		[]string{"query"},
 	)
-	failedPushedMetrics = prometheus_metrics.NewCounterVec(
-		prometheus_metrics.CounterOpts{
+	failedPushedMetrics = prometheusMetrics.NewCounterVec(
+		prometheusMetrics.CounterOpts{
 			Namespace: "prometheus_to_datadog",
 			Name:      "failed_pushed_metrics_total",
 			Help:      "Number of failed failed metric pushes.",
@@ -96,9 +68,9 @@ var (
 	)
 )
 
-func run_query(query Query, query_api prometheus.QueryAPI, when time.Time, statsd_client *statsd.Client) error {
+func runQuery(query Query, queryAPI prometheus.QueryAPI, when time.Time, statsdClient *statsd.Client) error {
 	var err error
-	results, err := query_api.Query(context.Background(), query.Query, when)
+	results, err := queryAPI.Query(context.Background(), query.Query, when)
 	if err != nil {
 		failedQueries.WithLabelValues(query.Query).Inc()
 		return err
@@ -120,28 +92,30 @@ func run_query(query Query, query_api prometheus.QueryAPI, when time.Time, stats
 
 		if name == "" {
 			failedPushedMetrics.WithLabelValues("invalid-name").Inc()
-			return fmt.Errorf("Invalid metric name from %v", query)
+			return fmt.Errorf("invalid metric name from %v", query)
 		}
 
-		post_pushed_metric := func(metric_type string) {
-			pushedMetrics.WithLabelValues(name, name, metric_type).Inc()
+		postPushedMetric := func(metricType string) {
+			pushedMetrics.WithLabelValues(name, name, metricType).Inc()
 		}
 
 		switch query.Type {
 		case Gauge:
-			err = statsd_client.Gauge(name, float64(sample.Value), tags, 1)
-			post_pushed_metric("gauge")
+			err = statsdClient.Gauge(name, float64(sample.Value), tags, 1)
+			postPushedMetric("gauge")
 		case Counter:
-			err = statsd_client.Count(name, int64(sample.Value), tags, 1)
-			post_pushed_metric("counter")
+			err = statsdClient.Count(name, int64(sample.Value), tags, 1)
+			postPushedMetric("counter")
 		case Histogram:
-			err = statsd_client.Histogram(name, float64(sample.Value), tags, 1)
-			post_pushed_metric("histogram")
+			err = statsdClient.Histogram(name, float64(sample.Value), tags, 1)
+			postPushedMetric("histogram")
 		case Milliseconds:
-			err = statsd_client.TimeInMilliseconds(name, float64(sample.Value), tags, 1)
-			post_pushed_metric("milliseconds")
+			err = statsdClient.TimeInMilliseconds(name, float64(sample.Value), tags, 1)
+			postPushedMetric("milliseconds")
+		case Set:
+			return fmt.Errorf("cannot handle query type 'set' (yet)")
 		default:
-			return fmt.Errorf("Can't handle %g", query.Type)
+			return fmt.Errorf("unknown query type %g", query.Type)
 		}
 		if err != nil {
 			failedPushedMetrics.WithLabelValues("failed-push").Inc()
@@ -151,47 +125,65 @@ func run_query(query Query, query_api prometheus.QueryAPI, when time.Time, stats
 	return err
 }
 
-func start_querying(ticker *time.Ticker, queries Queries, query_api prometheus.QueryAPI, statsd_client *statsd.Client) {
+func startQuerying(ticker *time.Ticker, queries Queries, queryAPI prometheus.QueryAPI, statsdClient *statsd.Client) {
 
 	go func() {
 		for now := range ticker.C {
 			for _, query := range queries {
-				run_query(query, query_api, now, statsd_client)
+				runQuery(query, queryAPI, now, statsdClient)
 			}
 		}
 	}()
 }
 
 func init() {
-	prometheus_metrics.MustRegister(pushedMetrics)
-	prometheus_metrics.MustRegister(failedQueries)
-	prometheus_metrics.MustRegister(failedPushedMetrics)
+	prometheusMetrics.MustRegister(pushedMetrics)
+	prometheusMetrics.MustRegister(failedQueries)
+	prometheusMetrics.MustRegister(failedPushedMetrics)
 }
 
 func main() {
-	flag.Var(&queries, "query", "Prometheus query (in form type:datadog_metric_name:prometheus_query). Can be specified multiple times.")
-	flag.Parse()
+	// Load config
+	viper.SetConfigName("prometheus_to_datadog")
+	viper.AddConfigPath("config/")
+	viper.AddConfigPath("etc/prometheus_to_datadog")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
 
-	statsd_client, err := statsd.New(*dogstatsd_addr)
+	statsdClient, err := statsd.New(viper.GetString("dogstatsd.address"))
 	if err != nil {
 		panic(err)
 	}
-	defer statsd_client.Close()
+	defer statsdClient.Close()
 
-	statsd_client.Namespace = "prometheus."
+	statsdClient.Namespace = viper.GetString("prometheus_to_datadog.namespace") + "."
 
-	prometheus_config := prometheus.Config{Address: *prometheus_addr}
-	prometheus_client, err := prometheus.New(prometheus_config)
+	prometheusConfig := prometheus.Config{Address: viper.GetString("prometheus.address")}
+	prometheusClient, err := prometheus.New(prometheusConfig)
 	if err != nil {
 		panic(err)
 	}
 
-	prometheus_query_api := prometheus.NewQueryAPI(prometheus_client)
+	prometheusQueryAPI := prometheus.NewQueryAPI(prometheusClient)
 
-	duration := time.Duration(*interval) * time.Second
-	ticker := time.NewTicker(duration)
-	start_querying(ticker, queries, prometheus_query_api, statsd_client)
+	queryInterval := time.Duration(viper.GetInt("prometheus_to_datadog.query_interval")) * time.Second
+	ticker := time.NewTicker(queryInterval)
 
-	http.Handle("/metrics", prometheus_metrics.Handler())
-	http.ListenAndServe(*listen_addr, nil)
+	// Load queries
+	queryFile, err := ioutil.ReadFile("config/queries.yaml")
+	if err != nil {
+		panic(fmt.Errorf("Fatal error queries file: %s \n", err))
+	}
+	var queries []Query
+	err = yaml.Unmarshal(queryFile, &queries)
+	if err != nil {
+		panic(fmt.Errorf("Fatal error parsing queries file: %s \n", err))
+	}
+
+	startQuerying(ticker, queries, prometheusQueryAPI, statsdClient)
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(viper.GetString("prometheus_to_datadog.listen_address"), nil)
 }
